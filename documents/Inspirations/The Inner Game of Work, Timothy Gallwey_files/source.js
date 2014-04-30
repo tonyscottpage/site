256c820d585b40386118a0482bbe93d17ffb525f
tonyscottpage <tonyscottpage@gmail.com>
2014-04-30 11:41:49 +0100

    Upload new documents file

---
+++ documents/Inspirations/The Inner Game of Work, Timothy Gallwey_files/source.js
@@ -0,0 +1,132 @@
+ 
+ 
+ /**
+  * Menu creation.
+  */
+ function fwLoadMenus() {
+   if (window.fw_menu_0) return;
+   window.fw_menu_0 = new Menu("root",197,19,"Verdana, Arial, Helvetica, sans-serif",12,"#000000","#ffffff","#FFFFCE","#000084");
+   fw_menu_0.addMenuItem("Training Home Page","location='http://www.corporatetrainer.co.uk/'");
+   fw_menu_0.addMenuItem("Training Needs Analysis","location='http://www.corporatetrainer.co.uk/training_needs_analysis.html'");
+   fw_menu_0.addMenuItem("Management Competencies","location='http://www.corporatetrainer.co.uk/management_competencies.html'");
+   fw_menu_0.addMenuItem("Management Development","location='http://www.corporatetrainer.co.uk/management_development.html'");
+   fw_menu_0.addMenuItem("Team Building","location='http://www.corporatetrainer.co.uk/team_building.html'");
+   fw_menu_0.addMenuItem("Presentation Skills","location='http://www.corporatetrainer.co.uk/presentation_skills.html'");
+   fw_menu_0.addMenuItem("Time Management","location='http://www.corporatetrainer.co.uk/time_management.html'");
+   fw_menu_0.addMenuItem("Stress Management","location='http://www.corporatetrainer.co.uk/stress_management.html'");
+   fw_menu_0.addMenuItem("Decision Making","location='http://www.corporatetrainer.co.uk/decision_making.html'");
+   fw_menu_0.addMenuItem("Negotiation Skills","location='http://www.corporatetrainer.co.uk/negotiation_skills.html'");
+    fw_menu_0.hideOnMouseOut=true;
+   window.fw_menu_1 = new Menu("root",184,19,"Verdana, Arial, Helvetica, sans-serif",12,"#000000","#ffffff","#FFFFCE","#000084");
+   fw_menu_1.addMenuItem("Coaching Home Page","location='http://www.corporatecoach.co.uk/'");
+   fw_menu_1.addMenuItem("Corporate Coaching","location='http://www.corporatecoach.co.uk/corporate_coaching.html'");
+   fw_menu_1.addMenuItem("Business Coaching","location='http://www.corporatecoach.co.uk/business_coaching.html'");
+   fw_menu_1.addMenuItem("Executive Coaching","location='http://www.corporatecoach.co.uk/executive_coaching.html'");
+   fw_menu_1.addMenuItem("Career Coaching","location='http://www.corporatecoach.co.uk/career_coaching.html'");
+   fw_menu_1.addMenuItem("Directors &amp; Boards","location='http://www.corporatecoach.co.uk/board_of_directors.html'");
+   fw_menu_1.addMenuItem("Personal Development","location='http://www.corporatecoach.co.uk/personal_development.html'");
+   fw_menu_1.addMenuItem("High Performance Teams","location='http://www.corporatecoach.co.uk/top_teams.html'");
+   fw_menu_1.addMenuItem("Leadership","location='http://www.corporatecoach.co.uk/leadership.html'");
+   fw_menu_1.addMenuItem("Executive Mentoring","location='http://www.corporatecoach.co.uk/executive_mentoring.html'");
+    fw_menu_1.hideOnMouseOut=true;
+   window.fw_menu_2 = new Menu("root",173,19,"Verdana, Arial, Helvetica, sans-serif",12,"#000000","#ffffff","#FFFFCE","#000084");
+   fw_menu_2.addMenuItem("Facilitation Home Page","location='http://www.corporatefacilitator.co.uk'");
+   fw_menu_2.addMenuItem("Corporate Retreats","location='http://www.corporatefacilitator.co.uk/retreats.html'");
+   fw_menu_2.addMenuItem("Away Days","location='http://www.corporatefacilitator.co.uk/awaydays.html'");
+   fw_menu_2.addMenuItem("Workshops","location='http://www.corporatefacilitator.co.uk/workshops.html'");
+   fw_menu_2.addMenuItem("Conferences","location='http://www.corporatefacilitator.co.uk/conferences.html'");
+   fw_menu_2.addMenuItem("Scenario Planning","location='http://www.corporatefacilitator.co.uk/scenarios.html'");
+   fw_menu_2.addMenuItem("Vision &amp; Mission","location='http://www.corporatefacilitator.co.uk/vision.html'");
+   fw_menu_2.addMenuItem("Strategy Planning","location='http://www.corporatefacilitator.co.uk/strategy.html'");
+   fw_menu_2.addMenuItem("Mediation","location='http://www.corporatefacilitator.co.uk/mediation.html'");
+   fw_menu_2.addMenuItem("Creativity &amp; Innovation","location='http://www.corporatefacilitator.co.uk/creativity.html'");
+    fw_menu_2.hideOnMouseOut=true;
+   window.fw_menu_3 = new Menu("root",196,19,"Verdana, Arial, Helvetica, sans-serif",12,"#000000","#ffffff","#FFFFCE","#000084");
+   fw_menu_3.addMenuItem("Consultancy Home Page","location='http://www.corporateconsultant.co.uk'");
+   fw_menu_3.addMenuItem("Change Management","location='http://www.corporateconsultant.co.uk/change_management.html'");
+   fw_menu_3.addMenuItem("Organisation Development","location='http://www.corporateconsultant.co.uk/organisation_development.html'");
+   fw_menu_3.addMenuItem("Learning Organisations","location='http://www.corporateconsultant.co.uk/learning_organisations.html'");
+   fw_menu_3.addMenuItem("Investors in People","location='http://www.corporateconsultant.co.uk/investors_in_people.html'");
+   fw_menu_3.addMenuItem("Benchmarking","location='http://www.corporateconsultant.co.uk/benchmarking.html'");
+   fw_menu_3.addMenuItem("Business Plans","location='http://www.corporateconsultant.co.uk/business_plans.html'");
+   fw_menu_3.addMenuItem("Distress Rescue","location='http://www.corporateconsultant.co.uk/distress_rescue.html'");
+   fw_menu_3.addMenuItem("Start-ups","location='http://www.corporateconsultant.co.uk/start-ups.html'");
+   fw_menu_3.addMenuItem("Internet Strategy","location='http://www.corporateconsultant.co.uk/internet_strategy.html'");
+    fw_menu_3.hideOnMouseOut=true;
+   window.fw_menu_4 = new Menu("root",138,19,"Verdana, Arial, Helvetica, sans-serif",12,"#000000","#ffffff","#FFFFCE","#000084");
+   fw_menu_4.addMenuItem("Main Home Page","location='http://www.brefigroup.co.uk/'");
+  	fw_menu_4.addMenuItem("Contact Us","location='http://www.brefigroup.co.uk/contact.php'");
+   fw_menu_4.addMenuItem("Newsletter","location='http://www.brefigroup.co.uk/newsletter.html'");
+   fw_menu_4.addMenuItem("Resources","location='http://www.brefigroup.co.uk/resources.html'");
+   fw_menu_4.addMenuItem("Media Coverage","location='http://www.brefigroup.co.uk/media_coverage.html'");
+   fw_menu_4.addMenuItem("Clients","location='http://www.brefigroup.co.uk/clients.html'");
+    fw_menu_4.hideOnMouseOut=true;
+
+   fw_menu_4.writeMenus();
+ } // fwLoadMenus()
+ 
+ 
+/**
+ * Creates the newsletter reminder popup.
+ */
+function Newsletter_Reminder()
+{
+	if ((GetCookie('newsletter_reminder') == null) && (GetCookie('newsletter_member') == null))
+	{
+		window.name = "mainWin";
+		Newsletter_Reminder = window.open("","","height=150,width=450,left=50,top=50,resize=yes");
+		if(parseInt(navigator.appVersion) >= 5)
+		{
+			Newsletter_Reminder.document.close();
+			Newsletter_Reminder.document.open("text/html");
+			Newsletter_Reminder.document.write(html);
+			Newsletter_Reminder.document.close();
+		}
+		else
+		{ 	Newsletter_Reminder.location.href = "http://www.brefigroup.co.uk/newsletter_reminder.html";	}
+		SetCookie('newsletter_reminder', 'done');
+	}
+}
+
+
+/**
+ * Sets a cookie.
+ */
+function SetCookie (name, value, expires)
+{
+	document.cookie = name + "=" + escape (value) + ((SetCookie.arguments.length > 2) ? SetCookie.arguments[2] : "")
+}
+
+
+/**
+ * Gets a cookie.
+ */
+function GetCookie (name)
+{
+	var arg = name + "=";
+	var alen = arg.length;
+	var clen = document.cookie.length;
+	var i = 0;
+
+	while (i < clen)
+	{
+		var j = i + alen;
+		if (document.cookie.substring(i, j) == arg)
+		return getCookieVal (j);
+		i = document.cookie.indexOf(" ", i) + 1;
+		if (i == 0) break;
+	}
+
+	return null;
+}
+
+/**
+ * Gets the value of a given cookie.
+ */
+function getCookieVal (offset)
+{
+	var endstr = document.cookie.indexOf (";", offset);
+	if (endstr == -1)
+	endstr = document.cookie.length;
+	return unescape(document.cookie.substring(offset, endstr));
+}
